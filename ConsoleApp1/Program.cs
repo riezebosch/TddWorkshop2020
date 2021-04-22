bf5214f8b96c77fcf2d26af5fe98868e2d874810
Manuel Riezebosch <mriezebosch@gmail.com>
2021-04-22 10:25:27 +0200

    feat(ioc): demo appliatie met service collection

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using TddWorkshop;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iban = Console.ReadLine();
+
+            var services = new ServiceCollection()
+                .AddSingleton<IBankCodesProvider, DatabaseBankCodesProvider>()
+                .AddSingleton<IbanValidatorNL>()
+                .BuildServiceProvider();
+
+            var validator = services.GetService<IbanValidatorNL>();
+            Console.WriteLine(validator.IsValid(iban));
+        }
+    }
+}
